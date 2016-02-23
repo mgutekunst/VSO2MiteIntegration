5f9ff8b077f56ecc0ca433e7f9677317b5a2665f
Manuel Gutekunst <maanu1@gmail.com>
2016-02-23 21:48:40 +0100

    refactored javascript into main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-// test
+// Use an IIFE to create an object that satisfies the IContributedMenuSource contract
+var menuContributionHandler = (function () {
+    "use strict";
+    return {
+	// This is a callback that gets invoked when a user clicks the newly contributed menu item
+	// The actionContext parameter contains context data surrounding the circumstances of this
+	// action getting invoked.
+	execute: function (actionContext) {
+	    alert("Hello, mite");
+	}
+    };
+}());
+
+// Associate the menuContributionHandler object with the "myAction" menu contribution from the manifest.
+VSS.register("home", menuContributionHandler);
